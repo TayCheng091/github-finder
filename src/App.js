4c6b22b85d212cfe850b10036602f5d339938a47
Tay <b1233967@gmail.com>
2022-09-13 00:02:28 +0800

    set spinner component and set github api id & key

--- src/App.js
+++ src/App.js
@@ -12,18 +12,17 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({
       loading: false,
       users: res.data,
     });
-
-    console.log("state = ", this.state);
   }
 
   render() {
-    console.log("1. render");
     return (
       <div className='App'>
         <Navbar />
